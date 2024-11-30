57f8646d3d00baa91b4d2cdc71745ae8f32c23f0
Andrey Duka <70843050+0895000@users.noreply.github.com>
2024-11-30 21:14:04 +0300

    feat: add last solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -601,10 +601,7 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
-}
-
+const getHypotenuse = (a, b) => Math.hypot(a, b);
 /**
  * Returns count of odd numbers from zero to the resulting number.
  * The resulting number is taken into account.
@@ -618,8 +615,15 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  if (number < 0) {
+    return 0;
+  }
+  let count = 0;
+  for (let i = 1; i <= number; i += 2) {
+    count += 1;
+  }
+  return count;
 }
 
 module.exports = {
